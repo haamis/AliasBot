0f3e998e0fe7b5f6dc62ef020d13a006b3e36a9f
Antti Virtanen <haama92@gmail.com>
2017-11-27 19:25:13 +0200

    Fixed reading token from file. Stupid newlines. Also changed to python 3.5 style async syntax.

--- AliasBot.py
+++ AliasBot.py
@@ -15,47 +15,47 @@ with open("aliases") as fh:
 	aliases = json.load(fh)
 
 with open("token") as fh:
-	token = fh.read()
+	token = fh.read().strip('\n')
 
 def updateAliases():
 	with open("aliases",'w') as fh:
 		json.dump(aliases, fh)
 
 @client.event
-@asyncio.coroutine
-def on_ready():
+#@asyncio.coroutine
+async def on_ready():
 	print('Logged in as')
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
 
 @client.event
-@asyncio.coroutine
-def on_message(message):
+#@asyncio.coroutine
+async def on_message(message):
 	if message.content.startswith("!alias"):
 		splitmessage = message.content.split(" ")
 		if splitmessage[1] == "add":
 			aliases[splitmessage[2]] = [" ".join(splitmessage[3:])]
 			updateAliases()
-			yield from client.send_message(message.channel, message.author.mention + " Alias " + splitmessage[2] + " added.")
+			await client.send_message(message.channel, message.author.mention + " Alias " + splitmessage[2] + " added.")
 		if splitmessage[1] == "remove":
 			aliases.pop(splitmessage[2])
 			updateAliases()
-			yield from client.send_message(message.channel, message.author.mention + " Alias " + splitmessage[2] + " removed.")
+			await client.send_message(message.channel, message.author.mention + " Alias " + splitmessage[2] + " removed.")
 		if splitmessage[1] == "list":
 			alias_list = ""
 			for a in aliases:
 				alias_list += a + " : " + aliases[a] + "\n"
-			yield from client.send_message(message.channel, message.author.mention + "\n" + "```" + alias_list + "```")
+			await client.send_message(message.channel, message.author.mention + "\n" + "```" + alias_list + "```")
 	elif message.content.startswith("!macro"):
 		messagelines = message.content.split("\n")
 		splitmessage = messagelines[0].split(" ")
 		if splitmessage[1] == "add":
 			aliases[splitmessage[2]] = messagelines[1:]
 			updateAliases()
-			yield from client.send_message(message.channel, aliases)
+			await client.send_message(message.channel, aliases)
 	else:
 		for line in aliases[message.content]:
-			yield from client.send_message(message.channel, line)
+			await client.send_message(message.channel, line)
 
 client.run(token)
